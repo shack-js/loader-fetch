dbccd5d6f2a00483685994ed7b54406b133759db
josh <postor@gmail.com>
2021-06-22 09:57:25 +0800

    basic

--- src/fetch.js
+++ src/fetch.js
@@ -1,14 +1,21 @@
+let _headers = {}
+
 export default async (url, params) => {
   const response = await fetch(url, {
     method: 'POST',
     mode: 'cors',
     cache: 'no-cache',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      ..._headers
     },
     body: JSON.stringify(params)
   })
   const { error, data } = await response.json()
   if (error) throw error
   return data
+}
+
+export const setHeaders = function (headers = {}) {
+  _headers = headers
 }
