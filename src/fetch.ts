fd86991179139f71215423a129384b8c46d479b1
postor <postor@gmail.com>
2023-03-29 21:40:33 +0800

    update fetch

--- src/fetch.ts
+++ src/fetch.ts
@@ -2,20 +2,19 @@
 let _headers = {}
 
 export const request = async (url: string, params: any) => {
-  let method = getMethod(url)
-
   const response = await fetch(
-    method != 'GET' ? url : appendQuery(url, params), {
-    method,
-    mode: 'cors',
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json',
-      ..._headers
-    },
-    body: method == 'GET' ? undefined : JSON.stringify(params)
-  })
-
+    url,
+    {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+        ..._headers
+      },
+      body: JSON.stringify(params)
+    }
+  )
   const { error, data } = await response.json();
   if (error)
     throw error
@@ -26,15 +25,4 @@ export const setHeaders = function (headers = {}) {
   _headers = headers
 }
 
-function getMethod(url: string) {
-  let basename = url.substring(url.lastIndexOf('/') + 1).toUpperCase()
-  for (let method of ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
-    if (basename.startsWith(method)) return method
-  }
-  return 'POST'
-}
-
-function appendQuery(url: string, params: any): any {
-  return `${url}?${params.map(x => `q[]=${encodeURIComponent(x)}`).join('&')}`
-}
 
